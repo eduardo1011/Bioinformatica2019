04d4814bb1a5fbadb7034e5719383e5cc53f5263
eduardo1011 <35233945+eduardo1011@users.noreply.github.com>
2019-05-24 17:21:28 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -33,6 +33,12 @@ Funciones, curso bioinformática
 
 10. go_file()
 
+11. run_Blast_Windows(x_p = '', file = '', db = '')
+
+12. run_Blast_Linux(x_p = '', file = '', db = '')
+
+13. run_blast_jupyter(x_p = '', file = '', db = '')
+
 =========================
 
 """
@@ -122,7 +128,8 @@ def word_cloud(df = DataFrame([]), size = 10):
             frec = list(np.repeat(re.sub(' |, |-','_', row.Term), row.Entry))
             frecuencia.append(' '.join(frec))
         text = ' '.join(frecuencia)
-        wave_mask = np.array(Image.open( "datos/ovalo.jpg"))
+        ovalo = urllib.request.urlretrieve('https://raw.githubusercontent.com/eduardo1011/Bioinformatica2019/master/ovalo.jpg', 'ovalo.jpg')
+        wave_mask = np.array(Image.open( "ovalo.jpg"))
         wordcloud = WordCloud(mask=wave_mask,collocations=False,max_font_size=1000,
                               #contour_color='firebrick',contour_width=10, stopwords = set(STOPWORDS),
                               max_words=2000, background_color="white").generate(text)
@@ -418,3 +425,115 @@ def go_file():
         # información de la base de datos
         print(urllib.request.urlopen(url).headers)
     return go1
+
+###
+def run_Blast_Windows(x_p = '', file = '', db = ''):
+    if file == '':
+        print('Intentar nuevamente')
+    else:
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/eduardo1011/Bioinformatica2019/master/runBlast.py', 'runBlast.py')
+        blast = open('runBlast.py','r')
+        blast = blast.read()
+        blast = re.sub('xxxxx',"'"+file+"'", blast)
+        blast = re.sub('yyyyy',"'"+x_p+"'", blast)
+        blast = re.sub('zzzzz',"'"+db+"'", blast)
+        save= open('run.py','w')
+        save.write(blast)
+        save.close()
+        runblast = os.system("start cmd /k python run.py")
+###
+def run_Blast_Linux(x_p = '', file = '', db = ''):
+    if file == '':
+        print('Intentar nuevamente')
+    else:
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/eduardo1011/Bioinformatica2019/master/runBlast.py', 'runBlast.py')
+        blast = open('runBlast.py','r')
+        blast = blast.read()
+        blast = re.sub('xxxxx',"'"+file+"'", blast)
+        blast = re.sub('yyyyy',"'"+x_p+"'", blast)
+        blast = re.sub('zzzzz',"'"+db+"'", blast)
+        blast = re.sub('#wwwww', 'print("")\nprint("Finalizado")\ntime.sleep(60)', blast)
+        save= open('run.py','w')
+        save.write(blast)
+        save.close()
+        os.system('gnome-terminal -e "python3 run.py"')
+###
+def run_blast_jupyter(x_p = '', file = '', db = ''):
+    if file == '':
+        print('Intentar nuevamente')
+    else:
+        print('\nCorriendo '+x_p+'\n')
+        # 
+        fasta1 = open(file,'r')
+        filename = file.split('.')[0]
+        name = x_p+'_'+filename.split('/')[-1]+'.tab'
+        #
+        print(x_p,'-db', db,'-query', file,'-evalue','1E-6','-outfmt\n',
+        "6 qacc sacc qlen slen length score bitscore evalue pident nident\n",
+        "mismatch positive gaps gapopen stitle",'-max_target_seqs','10\n',
+        '-max_hsps','1','-out', name+'\n')
+        #
+        fasta1 = fasta1.read()
+        # 
+        len(re.findall('>', fasta1))
+        ###
+        out_text = []
+        n = 0
+        dld = 0
+        tim = datetime.now()
+        xx = datetime.now()
+        secs = len(fasta1.split('>')) - 1
+        sin_resultados = []
+        for i in fasta1.split('>')[1:int(float(secs)) + 1]:
+            i = re.sub('\n$', '', i)
+            if re.search('[|]', i):
+                identifier = i.split('|')[1]
+            else:
+                identifier = re.search('\w+', i).group()
+            #identifier = re.search('\w+', i).group()
+            ###
+            save1= open(identifier,'w')
+            save1.write('>'+i)
+            save1.close()
+            ###
+            blast = subprocess.call([x_p,'-db',db,'-query', identifier,'-evalue','1E-6','-outfmt',
+                                "6 qacc sacc qlen slen length score bitscore evalue pident nident mismatch positive gaps gapopen stitle",
+                                '-max_target_seqs','10','-max_hsps','1','-out', identifier+'.txt'])
+            ###
+            out = open(identifier+'.txt','r')
+            out = out.read()
+            n += 1
+            if len(out) == 0:
+                out_bytes = len(out)
+                sin_resultados.append('Secuencia '+str(n)+' | '+str(identifier))
+                os.remove(identifier)
+                os.remove(identifier+'.txt')
+                continue
+            else:
+                out_text.append(pd.read_csv(StringIO(out),sep='\t',header=None))
+                out_bytes = len(out)
+                ###
+                dif = max([i for i in range(1, out_bytes+10,10)]) - out_bytes
+                total_length = int(out_bytes)
+                dld += out_bytes
+                dl = 0
+                for dat in [i for i in range(1, out_bytes+10,10)]:
+                    tim = datetime.now() - xx
+                    dl = dat - dif
+                    done = int(30 * dl / total_length)
+                    sys.stdout.write('\rSecuencia '+str(n)+' | %s | %s [%s%s] Bytes %s' % ('{}'.format(tim).split('.')[0], identifier,'■' * done, ' ' * (30 - done), dl)) 
+                    sys.stdout.flush()
+            os.remove(identifier)
+            os.remove(identifier+'.txt')
+        ###
+        resultados_blast = pd.concat(out_text)
+        header = ('qacc','Entry','qlen','slen','length','score','bitscore','evalue','pident','nident',
+                          'mismatch','positive','gaps','gapopen','stitle')
+        resultados_blast.columns = header
+        resultados_blast.to_csv(name, sep = '\t',index = None)
+        print('\n\nTiempo: {}'.format(tim).split('.')[0], '(h:m:s)')
+        print('\nResultado: ', name, '\n')
+        print('Secuencias sin resultados: '+str(len(sin_resultados)))
+        #for i in sin_resultados:
+        #    print(i)
+###
